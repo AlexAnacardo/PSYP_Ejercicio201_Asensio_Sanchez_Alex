c5d87861c2c21e8b20d5490d4c4d1b3125b2371f
Alex <alexasensio04clase@gmail.com>
2025-11-07 14:00:14 +0100

    Avances ej 4

--- src/main/java/Ej04/Hilo.java
+++ src/main/java/Ej04/Hilo.java
@@ -1,5 +1,43 @@
 package Ej04;
 
+import java.util.Random;
+
 public class Hilo extends Thread{
 	
+	String nombre;
+	int repeticiones;
+	Random random;
+	
+	public Hilo(String nombre, int repeticiones) {
+		super();
+		this.nombre = nombre;
+		this.repeticiones = repeticiones;
+		random = new Random();
+	}
+	
+	public void run() {
+		
+		
+		
+		
+		try {
+			for(int i=0; i<=repeticiones; i++) {
+				if(this.getThreadGroup().activeCount()>2) {
+					System.out.println(nombre);
+					System.out.println(this.getThreadGroup().activeCount());
+					sleep(random.nextInt(4000)+1);
+				}
+				else {
+					System.out.println(nombre);
+					System.out.println(this.getThreadGroup().activeCount());
+					System.out.println(nombre+": Soy el último superviviente");
+					sleep(random.nextInt(4000)+1);
+				}
+				
+			}			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/Ej04/Main.java
+++ src/main/java/Ej04/Main.java
@@ -1,13 +1,21 @@
 package Ej04;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
-	public Main() {
-		
-	}
-
 	public static void main(String[] args) {
-
+		
+		ArrayList<Hilo> hilos = new ArrayList<>();
+		
+		for(int i=0; i<=4; i++) {
+			hilos.add(new Hilo("Hilo"+(i+1), 5));
+		}
+		
+		for(Hilo h: hilos) {			
+			h.start();		
+		}
 	}
 
 }
